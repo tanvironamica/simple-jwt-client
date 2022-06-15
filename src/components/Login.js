25b644165370dbe23c4be085251410a9006c2534
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-06-15 10:42:37 +0600

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const inputStyle = {
+    width: '100%',
+    height: '30px',
+    marginBottom: '10px'
+}
+
+const Login = () => {
+    const navigate = useNavigate();
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.success){
+                localStorage.setItem('accessToken', data.accessToken);
+                navigate('/orders');
+            }
+            console.log(data);
+        })
+    }
+
+    return (
+        <form onSubmit={handleLogin} style={{ width: '50%', margin: '20px auto' }}>
+            <input style={inputStyle} type="email" name="email" placeholder='Email' />
+            <br />
+            <input style={inputStyle} type="password" name="password" placeholder='Password' />
+            <br />
+            <input type="submit" value="Login" />
+        </form>
+    );
+};
+
+export default Login;
---
+++ src/components/Orders.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Orders = () => {
+    const navigate = useNavigate();
+    const [orders, setOrders] = useState([]);
+
+    useEffect( () =>{
+        fetch('http://localhost:5000/orders', {
+            method: 'GET',
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => {
+            if(res.status === 401 || res.status === 403){
+                navigate('/login');
+            } 
+            return res.json()
+        })
+        .then(data =>{
+            console.log(data);
+            setOrders(data);
+        })
+    }, [])
+    return (
+        <div>
+            <h1>This is orders page: {orders.length}</h1>
+        </div>
+    );
+};
+
+export default Orders;
